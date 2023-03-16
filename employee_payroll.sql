107665f873c81c46d254b563292e20de4cc16835
Subhash Barela <barelasubhash16@gmail.com>
2023-03-16 12:58:05 +0530

    insert the data into table

--- employee_payroll.sql
+++ employee_payroll.sql
@@ -1,10 +1,8 @@
-create database payroll_service;
+select * from employee_payroll
 
-create table employee_payroll(id int not null identity,
-name varchar(50) not null,
-salary   bigint NOT NULL,
-start  date not null,
-primary key (id)
-)
+insert into employee_payroll(name, salary,start)
+values('raj',200000,'2019-01-03'),
+('vinnu',300000,'2020-04-05'),
+('charlis',430000,'2021-09-14')
 
 select * from employee_payroll
