bf46e01292551ad0fef763a97433425ea9973b71
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-03-16 12:27:59 +0530

    Update employee_payroll.sql

--- employee_payroll.sql
+++ employee_payroll.sql
@@ -1,4 +1,7 @@
 create database payroll_service;
+--see the databases ...using show databases
+
+--choose database where we create table using ....use database name
 
 create table employee_payroll(id int not null identity,
 name varchar(50) not null,
